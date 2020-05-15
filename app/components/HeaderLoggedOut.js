c5e022a06a56e843185c3ab1f8862693e5566b92
amihut <amihut@EN610723>
2020-05-15 15:33:15 +0300

    handle login and logout state

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 
-function HeaderLoggedOut() {
+function HeaderLoggedOut( props ) {
   const [ username, setUsername ] = useState();
   const [ password, setPassword ] = useState();
 
@@ -14,7 +14,7 @@ function HeaderLoggedOut() {
       } )
       if ( response.data ) {
         console.log( response.data )
-
+        props.setLoggedIn( true );
       } else {
         console.log( 'Incorrect user/password' )
       }
