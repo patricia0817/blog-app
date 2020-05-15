50dd993b61520528bd0c3a4e50737f66ee86ae97
amihut <amihut@EN610723>
2020-05-15 13:08:07 +0300

    update document title based on routes

--- app/components/Container.js
+++ app/components/Container.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 
 function Container( props ) {
   return (
-    <div className={ 'container py-md-5 ' + ( props.wide ? '' : 'cotainer--narrow' ) }>
+    <div className={ 'container py-md-5 ' + ( props.wide ? '' : 'container--narrow' ) }>
       { props.children }
     </div>
   )
