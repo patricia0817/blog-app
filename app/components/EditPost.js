f65dc126b5e8653139880c219e753f41b0e5a32a
amihut <amihut@EN610723>
2020-05-20 16:12:30 +0300

    add form validation

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -38,18 +38,34 @@ function EditPost() {
         return;
       case 'titleChange':
         draft.title.value = action.value
+        draft.title.hasErrors = false
         return;
       case 'bodyChange':
         draft.body.value = action.value
+        draft.body.hasErrors = false
         return;
       case 'submitRequest':
-        draft.sendCount++;
+        if ( !draft.title.hasErrors && !draft.body.hasErrors ) {
+          draft.sendCount++;
+        }
         return;
       case 'saveRequestStarted':
         draft.isSaving = true
         return;
       case 'saveRequestFinish':
         draft.isSaving = false
+        return;
+      case 'titleRules':
+        if ( !action.value.trim() ) {
+          draft.title.hasErrors = true;
+          draft.title.message = 'You must provide a title.'
+        }
+        return;
+      case 'bodyRules':
+        if ( !action.value.trim() ) {
+          draft.body.hasErrors = true;
+          draft.body.message = 'You must provide body content.'
+        }
         return;
     }
   }
@@ -58,6 +74,8 @@ function EditPost() {
 
   function submitHandler( e ) {
     e.preventDefault();
+    dispatch( { type: 'titleRules', value: state.title.value } )
+    dispatch( { type: 'bodyRules', value: state.body.value } )
     dispatch( { type: 'submitRequest' } )
   }
 
@@ -113,13 +131,23 @@ function EditPost() {
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input onChange={ e => dispatch( { type: "titleChange", value: e.target.value } ) } value={ state.title.value } autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          <input onBlur={ e => dispatch( { type: "titleRules", value: e.target.value } ) } onChange={ e => dispatch( { type: "titleChange", value: e.target.value } ) } value={ state.title.value } autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          { state.title.hasErrors &&
+            <div className="alert alert-danger small liveValidateMessage">
+              { state.title.message }
+            </div>
+          }
         </div>
         <div className="form-group">
           <label htmlFor="post-body" className="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
-          <textarea onChange={ e => dispatch( { type: "bodyChange", value: e.target.value } ) } name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={ state.body.value } />
+          <textarea onBlur={ e => dispatch( { type: 'bodyRules', value: e.target.value } ) } onChange={ e => dispatch( { type: "bodyChange", value: e.target.value } ) } name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={ state.body.value } />
+          { state.body.hasErrors &&
+            <div className="alert alert-danger small liveValidateMessage">
+              { state.body.message }
+            </div>
+          }
         </div>
         <button className="btn btn-primary" disabled={ state.isSaving }>
           { state.isSaving ? 'Saving...' : 'Save Updates' }
