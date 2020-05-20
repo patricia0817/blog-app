fbfa365b81ac1c6683bdb740a7c5536f55aabd72
amihut <amihut@EN610723>
2020-05-20 14:15:30 +0300

    adding tooltips on hover for delete and edit post actions

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -3,6 +3,7 @@ import Page from './Page'
 import { useParams, Link } from 'react-router-dom'
 import Axios from 'axios'
 import ReactMarkdown from 'react-markdown'
+import ReactTooltip from 'react-tooltip'
 
 import LoadingDotsIcon from './LoadingDotsIcon'
 
@@ -44,8 +45,15 @@ function ViewSinglePost() {
       <div className="d-flex justify-content-between">
         <h2>{ post.title }</h2>
         <span className="pt-2">
-          <a href="#" className="text-primary mr-2" title="Edit"><i className="fas fa-edit"></i></a>
-          <a className="delete-post-button text-danger" title="Delete"><i className="fas fa-trash"></i></a>
+          <a href="#" data-tip="Edit" data-for="edit" className="text-primary mr-2">
+            <i className="fas fa-edit"></i>
+          </a>
+          <ReactTooltip id="edit" className="custom-tooltip" />
+          { " " }
+          <a data-tip="Delete" data-for="delete" className="delete-post-button text-danger">
+            <i className="fas fa-trash"></i>
+          </a>
+          <ReactTooltip id="delete" className="custom-tooltip" />
         </span>
       </div>
 
