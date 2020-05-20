2b8b8c450bc02bf0ce4284aa74963402b3751d06
amihut <amihut@EN610723>
2020-05-20 10:38:25 +0300

    load posts by author

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -5,6 +5,7 @@ import Axios from 'axios'
 import StateContext from '../StateContext'
 
 import Page from './Page'
+import ProfilePosts from './ProfilePosts'
 
 function Profile() {
   const appState = useContext( StateContext )
@@ -51,21 +52,7 @@ function Profile() {
           Following: { profileData.counts.followingCount }
         </a>
       </div>
-
-      <div className="list-group">
-        <a href="#" className="list-group-item list-group-item-action">
-          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #2</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #3</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-      </div>
+      <ProfilePosts />
     </Page>
   )
 }
