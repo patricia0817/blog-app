56db4e42becf036e242c11eab48df8d12a479584
amihut <amihut@EN610723>
2020-05-20 14:53:58 +0300

    update post component

--- app/Main.js
+++ app/Main.js
@@ -20,6 +20,7 @@ import CreatePost from './components/CreatePost'
 import ViewSinglePost from './components/ViewSinglePost'
 import FlashMessages from './components/FlashMessages'
 import Profile from './components/Profile';
+import EditPost from './components/EditPost';
 
 function Main() {
   const initialState = {
@@ -74,9 +75,12 @@ function Main() {
             </Route>
             <Route path='/' exact>
               { state.loggedIn ? <Home /> : <HomeGuest /> }}
-        </Route>
-            <Route path='/post/:id'>
+            </Route>
+            <Route path='/post/:id' exact>
               <ViewSinglePost />
+            </Route>
+            <Route path='/post/:id/edit' exact>
+              <EditPost />
             </Route>
             <Route path='/create-post'>
               <CreatePost />
