c0c482c70865b7e1fb699ed94d35b9a201d88cd2
amihut <amihut@EN610723>
2020-05-21 16:03:45 +0300

    profile followers and following results

--- app/components/ProfileFollow.js
+++ app/components/ProfileFollow.js
@@ -4,7 +4,7 @@ import { useParams, Link } from 'react-router-dom'
 
 import LoadingDotsIcon from './LoadingDotsIcon'
 
-function ProfilePosts() {
+function ProfileFollow( props ) {
   const [ isLoading, setIsLoading ] = useState( true );
   const [ posts, setPosts ] = useState( [] );
   const { username } = useParams();
@@ -14,7 +14,7 @@ function ProfilePosts() {
 
     async function fetchPosts() {
       try {
-        const response = await Axios.get( `/profile/${ username }/posts`, { cancelToken: ourRequest.token } )
+        const response = await Axios.get( `/profile/${ username }/${ props.action }`, { cancelToken: ourRequest.token } )
         setPosts( response.data )
         setIsLoading( false );
       } catch ( e ) {
@@ -25,18 +25,16 @@ function ProfilePosts() {
     return () => {
       ourRequest.cancel();
     }
-  }, [ username ] )
+  }, [ username, props.action ] )
 
   if ( isLoading ) return <LoadingDotsIcon />
   return (
     <div className="list-group">
-      { posts.map( ( post ) => {
-        const date = new Date( post.createdDate );
-        const dateFormatted = `${ date.getMonth() + 1 }/${ date.getDate() }/${ date.getFullYear() }`
+      { posts.map( ( follower, index ) => {
+
         return (
-          <Link key={ post._id } to={ `/post/${ post._id }` } className="list-group-item list-group-item-action">
-            <img className="avatar-tiny" src={ post.author.avatar } /> <strong>{ post.title }</strong>{ " " }
-            <span className="text-muted small">on { dateFormatted } </span>
+          <Link key={ index } to={ `/profile/${ follower.username }` } className="list-group-item list-group-item-action">
+            <img className="avatar-tiny" src={ follower.avatar } /> { follower.username }
           </Link>
         )
       } ) }
@@ -44,4 +42,4 @@ function ProfilePosts() {
   )
 }
 
-export default ProfilePosts
+export default ProfileFollow
--- app/components/ProfileFollowing.js
+++ app/components/ProfileFollowing.js
@@ -4,7 +4,7 @@ import { useParams, Link } from 'react-router-dom'
 
 import LoadingDotsIcon from './LoadingDotsIcon'
 
-function ProfilePosts() {
+function ProfileFollowing() {
   const [ isLoading, setIsLoading ] = useState( true );
   const [ posts, setPosts ] = useState( [] );
   const { username } = useParams();
@@ -14,7 +14,7 @@ function ProfilePosts() {
 
     async function fetchPosts() {
       try {
-        const response = await Axios.get( `/profile/${ username }/posts`, { cancelToken: ourRequest.token } )
+        const response = await Axios.get( `/profile/${ username }/following`, { cancelToken: ourRequest.token } )
         setPosts( response.data )
         setIsLoading( false );
       } catch ( e ) {
@@ -28,20 +28,24 @@ function ProfilePosts() {
   }, [ username ] )
 
   if ( isLoading ) return <LoadingDotsIcon />
+
+
+  console.log( appState.user.userName );
+  console.log( username )
   return (
-    <div className="list-group">
-      { posts.map( ( post ) => {
-        const date = new Date( post.createdDate );
-        const dateFormatted = `${ date.getMonth() + 1 }/${ date.getDate() }/${ date.getFullYear() }`
-        return (
-          <Link key={ post._id } to={ `/post/${ post._id }` } className="list-group-item list-group-item-action">
-            <img className="avatar-tiny" src={ post.author.avatar } /> <strong>{ post.title }</strong>{ " " }
-            <span className="text-muted small">on { dateFormatted } </span>
-          </Link>
-        )
-      } ) }
-    </div>
+    < div className="list-group" >
+      {
+        posts.map( ( follower, index ) => {
+
+          return (
+            <Link key={ index } to={ `/profile/${ follower.username }` } className="list-group-item list-group-item-action">
+              <img className="avatar-tiny" src={ follower.avatar } /> { follower.username }
+            </Link>
+          )
+        } )
+      }
+    </ div >
   )
 }
 
-export default ProfilePosts
+export default ProfileFollowing
