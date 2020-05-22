96138bdbcd9101d2814ec1f838cd07ae1041b87e
amihut <amihut@EN610723>
2020-05-22 10:27:49 +0300

    homepage post feed

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -3,6 +3,7 @@ import Axios from 'axios'
 import { useParams, Link } from 'react-router-dom'
 
 import LoadingDotsIcon from './LoadingDotsIcon'
+import Post from './Post';
 
 function ProfilePosts() {
   const [ isLoading, setIsLoading ] = useState( true );
@@ -31,14 +32,8 @@ function ProfilePosts() {
   return (
     <div className="list-group">
       { posts.map( ( post ) => {
-        const date = new Date( post.createdDate );
-        const dateFormatted = `${ date.getMonth() + 1 }/${ date.getDate() }/${ date.getFullYear() }`
-        return (
-          <Link key={ post._id } to={ `/post/${ post._id }` } className="list-group-item list-group-item-action">
-            <img className="avatar-tiny" src={ post.author.avatar } /> <strong>{ post.title }</strong>{ " " }
-            <span className="text-muted small">on { dateFormatted } </span>
-          </Link>
-        )
+
+        return <Post noAuthor={ true } post={ post } key={ post._id } />
       } ) }
     </div>
   )
--- app/components/Search.js
+++ app/components/Search.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { useImmer } from 'use-immer'
 import Axios from 'axios'
 import DispatchContext from '../DispatchContext'
+import Post from './Post'
 
 function Search() {
   const appDispatch = useContext( DispatchContext )
@@ -95,14 +96,7 @@ function Search() {
               <div className="list-group shadow-sm">
                 <div className="list-group-item active"><strong>Search Results</strong> ({ state.results.length } { state.results.length > 1 ? "items" : "item" } found)</div>
                 { state.results.map( ( post ) => {
-                  const date = new Date( post.createdDate );
-                  const dateFormatted = `${ date.getMonth() + 1 }/${ date.getDate() }/${ date.getFullYear() }`
-                  return (
-                    <Link onClick={ () => appDispatch( { type: "closeSearch" } ) } key={ post._id } to={ `/post/${ post._id }` } className="list-group-item list-group-item-action">
-                      <img className="avatar-tiny" src={ post.author.avatar } /> <strong>{ post.title }</strong>{ " " }
-                      <span className="text-muted small"> by { post.author.username } on { dateFormatted } </span>
-                    </Link>
-                  )
+                  return <Post onClick={ () => appDispatch( { type: "closeSearch" } ) } post={ post } key={ post._id } />
                 } ) }
               </div>
             ) }
