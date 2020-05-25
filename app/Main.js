29b55c3f3c97f5bbf43144bd1eae6e7e8e1c18ca
amihut <amihut@EN610723>
2020-05-25 15:30:17 +0300

    check if tocken has expired

--- app/Main.js
+++ app/Main.js
@@ -75,6 +75,27 @@ function Main() {
 
   const [ state, dispatch ] = useImmerReducer( ourReducer, initialState );
 
+  //check if token has expired or not on first render
+  useEffect( () => {
+    if ( state.loggedIn ) {
+      const ourRequest = Axios.CancelToken.source()
+      async function fetchResults() {
+        try {
+          const response = await Axios.post( '/checkToken', { token: state.user.token }, { cancelToken: ourRequest.token } )
+          if ( !response.data ) {
+            dispatch( { type: 'logout' } )
+            dispatch( { type: 'flashMessage', value: 'Your session has expired. Please log in again.' } )
+          }
+        } catch ( e ) {
+          console.log( 'There was a problem or the request was cancelled' )
+        }
+      }
+      fetchResults()
+
+      return () => ourRequest.cancel()
+    }
+  }, [] )
+
   useEffect( () => {
     if ( state.loggedIn ) {
       localStorage.setItem( 'complexappToken', state.user.token );
