9325bd8399d95eb5098b8aae1866b13013c51895
amihut <amihut@EN610723>
2020-05-26 11:50:01 +0300

    add case for profile not found situation

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -10,6 +10,7 @@ import ProfilePosts from './ProfilePosts'
 import ProfileFollowers from './ProfileFollowers'
 import ProfileFollowing from './ProfileFollowing'
 import ProfileFollow from './ProfileFollow'
+import NotFound from './NotFound'
 
 function Profile() {
   const appState = useContext( StateContext )
@@ -23,6 +24,7 @@ function Profile() {
       profileUsername: '...',
       profileAvatar: 'https://gravatar.com/avatar/placeholder?s=128',
       isFollowing: false,
+      exists: true,
       counts: {
         postCount: '',
         followerCount: '',
@@ -37,9 +39,16 @@ function Profile() {
     async function fetchData() {
       try {
         const response = await Axios.post( `/profile/${ username }`, { token: appState.user.token }, { cancelToken: ourRequest.token } )
-        setState( ( draft ) => {
-          draft.profileData = response.data
-        } )
+        if ( response.data ) {
+          setState( ( draft ) => {
+            draft.profileData = response.data
+            draft.profileData.exists = true
+          } )
+        } else {
+          setState( ( draft ) => {
+            draft.profileData.exists = false
+          } )
+        }
       } catch ( e ) {
         console.log( 'There was a problem or the request was cancelled' )
       }
@@ -116,42 +125,42 @@ function Profile() {
     } )
   }
 
-
   return (
-    <Page title="Profile Screen">
-      <h2>
-        <img className="avatar-small" src={ state.profileData.profileAvatar } /> { state.profileData.profileUsername }
-        { appState.loggedIn && !state.profileData.isFollowing && appState.user.username !== state.profileData.profileUsername && state.profileData.profileUsername !== '...' && (
-          <button onClick={ startFollowing } disabled={ state.followActionLoading } className="btn btn-primary btn-sm ml-2">Follow <i className="fas fa-user-plus"></i></button>
-        ) }
-        { appState.loggedIn && state.profileData.isFollowing && appState.user.username !== state.profileData.profileUsername && state.profileData.profileUsername !== '...' && (
-          <button onClick={ stopFollowing } disabled={ state.followActionLoading } className="btn btn-danger btn-sm ml-2">Stop Following <i className="fas fa-user-times"></i></button>
-        ) }
-      </h2>
+    !state.profileData.exists ? <NotFound /> :
+      <Page title="Profile Screen">
+        <h2>
+          <img className="avatar-small" src={ state.profileData.profileAvatar } /> { state.profileData.profileUsername }
+          { appState.loggedIn && !state.profileData.isFollowing && appState.user.username !== state.profileData.profileUsername && state.profileData.profileUsername !== '...' && (
+            <button onClick={ startFollowing } disabled={ state.followActionLoading } className="btn btn-primary btn-sm ml-2">Follow <i className="fas fa-user-plus"></i></button>
+          ) }
+          { appState.loggedIn && state.profileData.isFollowing && appState.user.username !== state.profileData.profileUsername && state.profileData.profileUsername !== '...' && (
+            <button onClick={ stopFollowing } disabled={ state.followActionLoading } className="btn btn-danger btn-sm ml-2">Stop Following <i className="fas fa-user-times"></i></button>
+          ) }
+        </h2>
 
-      <div className="profile-nav nav nav-tabs pt-2 mb-4">
-        <NavLink exact to={ `/profile/${ state.profileData.profileUsername }` } className="nav-item nav-link">
-          Posts: { state.profileData.counts.postCount }
-        </NavLink>
-        <NavLink to={ `/profile/${ state.profileData.profileUsername }/followers` } className="nav-item nav-link">
-          Followers: { state.profileData.counts.followerCount }
-        </NavLink>
-        <NavLink to={ `/profile/${ state.profileData.profileUsername }/following` } className="nav-item nav-link">
-          Following: { state.profileData.counts.followingCount }
-        </NavLink>
-      </div>
-      <Switch>
-        <Route exact path="/profile/:username">
-          <ProfilePosts />
-        </Route>
-        <Route path="/profile/:username/followers">
-          <ProfileFollow action="followers" />
-        </Route>
-        <Route path="/profile/:username/following">
-          <ProfileFollow action="following" />
-        </Route>
-      </Switch>
-    </Page>
+        <div className="profile-nav nav nav-tabs pt-2 mb-4">
+          <NavLink exact to={ `/profile/${ state.profileData.profileUsername }` } className="nav-item nav-link">
+            Posts: { state.profileData.counts.postCount }
+          </NavLink>
+          <NavLink to={ `/profile/${ state.profileData.profileUsername }/followers` } className="nav-item nav-link">
+            Followers: { state.profileData.counts.followerCount }
+          </NavLink>
+          <NavLink to={ `/profile/${ state.profileData.profileUsername }/following` } className="nav-item nav-link">
+            Following: { state.profileData.counts.followingCount }
+          </NavLink>
+        </div>
+        <Switch>
+          <Route exact path="/profile/:username">
+            <ProfilePosts />
+          </Route>
+          <Route path="/profile/:username/followers">
+            <ProfileFollow action="followers" />
+          </Route>
+          <Route path="/profile/:username/following">
+            <ProfileFollow action="following" />
+          </Route>
+        </Switch>
+      </Page>
   )
 }
 
